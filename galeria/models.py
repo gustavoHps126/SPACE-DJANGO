cbde4dd3dafbd2e6e979ea804447f0bbdca18e6c
gustavoHps126 <gustavo.pf.126@gmail.com>
2026-01-15 00:08:02 -0300

    Adição de pesquisa

--- galeria/models.py
+++ galeria/models.py
@@ -14,10 +14,10 @@ class Fotografia(models.Model):
     nome = models.CharField(max_length=50, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True)
     categoria = models.CharField(max_length=50, choices=opcoes_categoria, default='')
     publicada = models.BooleanField(default=False)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
-        return f'Fotografia [nome={self.nome}]'
+        return self.nome
--- setup/settings.py
+++ setup/settings.py
@@ -125,4 +125,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'setup/static')
 ]
 
+#media
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
