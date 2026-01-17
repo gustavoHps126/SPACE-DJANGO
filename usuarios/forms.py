ea43c11600d7108f43a7b25cd64d469304772a88
gustavoHps126 <gustavo.pf.126@gmail.com>
2026-01-16 23:30:37 -0300

    Telas de login e cadastro

---
+++ usuarios/forms.py
@@ -0,0 +1,73 @@
+from django import forms
+
+class LoginForms(forms.Form):
+    nome_login=forms.CharField(
+        label='Nome do Usuario',
+        required=True,
+        max_length=50,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex.:Gustavo Henrique'
+            }
+        )
+    )
+    senha=forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=25,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha'
+            }
+        )
+    )
+
+class CadastroForms(forms.Form):
+    nome_login=forms.CharField(
+        label='Nome de cadastro',
+        required=True,
+        max_length=50,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex.:Gustavo Henrique'
+            }
+        )
+    )
+    email=forms.EmailField(
+        label='Email',
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex.:gu2026@xpto.com'
+            }
+        )
+    )
+    senha_1=forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=25,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha'
+            }
+        )
+    )
+
+
+    senha_2=forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=25,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha novamente'
+            }
+        )
+    )
